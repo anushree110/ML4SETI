57ba313e8dbe44c2c228f9b59f2fd13790313ebe
Mansi Rankawat <mansirankawat19@gmail.com>
2017-06-11 17:51:58 +0000

    Eval and create_lmdb scripts

--- explorer_code/create_lmdb.py
+++ explorer_code/create_lmdb.py
@@ -1,27 +1,30 @@
-mport numpy as np
+import numpy as np
+import pdb
 import lmdb
 import caffe
-file_path = ''
+import cv2
+from PIL import Image
+file_path = '/home/nimbix/data/explorers/valid_class_label_map.csv'
 N = 1000
 f = open(file_path)
 map_size =  10e12
 
-env = lmdb.open('train_lmdb', map_size=map_size)
+env = lmdb.open('val_lmdb', map_size=map_size)
 count = 0
 with env.begin(write=True) as txn:
    for line in f:
       line = line.strip('\n')
-      file_info = line.strip(",")
-      pdb.set_trace()
+      file_info = line.split(",")
       img = cv2.imread(file_info[0], cv2.IMREAD_COLOR)
-      pdb.set_trace()
-      img = img.transpose((2, 1, 0))
+      #img = np.array(Image.open(file_info[0]).convert('RGB'))
+      bgr_img = img.transpose((2, 0, 1))
       label = file_info[1]
       datum = caffe.proto.caffe_pb2.Datum()
-      datum.channels = img.shape[0]
-      datum.height = img.shape[1]
-      datum.width = img.shape[2]
-      datum.label = int(file_info[1])
+      datum.channels = bgr_img.shape[0]
+      datum.height = bgr_img.shape[1]
+      datum.width = bgr_img.shape[2]
+      datum.data = bgr_img.tobytes()
+      datum.label = int(file_info[1]) 
       str_id = '{:08}'.format(count)
       count = count + 1
       txn.put(str_id.encode('ascii'), datum.SerializeToString())
---
+++ explorer_code/eval.py
@@ -0,0 +1,26 @@
+import caffe
+import os
+import numpy as np
+import sys
+import cv2
+import csv
+import glob
+net = caffe.Net('/home/nimbix/caffe/models/bvlc_alexnet/deploy.prototxt',
+'/home/nimbix/caffe/models/bvlc_alexnet/snapshots/iter_iter_7000.caffemodel.h5', caffe.TEST)
+csv_file = open('/home/nimbix/data/explorers/filename_uuid_map_test_data.csv')
+test_pngs = glob.glob('/home/nimbix/data/explorers/test_data_png/*.png')
+file_f = open('/home/nimbix/data/explorers/result3.csv', 'w')
+for line in csv_file:
+    line = line.strip('\n')
+    file_info = line.split(',')
+    img = cv2.imread(file_info[0], cv2.IMREAD_COLOR)
+    img = img.astype(float)
+    img -= np.array([125.0, 125.0, 125.0])
+    img = img.transpose((2, 0, 1))
+    net.blobs['data'].reshape(1, *img.shape)
+    net.blobs['data'].data[...]= img
+    output = net.forward()
+    result = output['prob']
+    writer = csv.writer(file_f)
+    writer.writerow([file_info[1], result[0][3], result[0][2], result[0][0], result[0][1]])
+    
